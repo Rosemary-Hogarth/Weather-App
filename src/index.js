7362212b06f37f21892cf75fe0baf7b9e46591b4
Rosemary-Hogarth <144120206+Rosemary-Hogarth@users.noreply.github.com>
2023-09-30 16:33:49 +0200

    changed js back to original date and time

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
-function formatDay(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  //let h1 = document.querySelector("h1");
+let currentDate = new Date();
+let h1 = document.querySelector("h1");
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+let time = currentDate.getHours();
+let minutes = currentDate.getMinutes();
 
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+if (minutes < 10) {
+  minutes = `0${minutes}`;
 }
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
-}
+h1.innerHTML = `${day} ${time}:${minutes}`;
 
 function displayTemp(response) {
   let temp = Math.round(response.data.main.temp);
