e8c40f8fdd0cfb9b28b60e76a405bf54b81cb7b6
Rosemary-Hogarth <144120206+Rosemary-Hogarth@users.noreply.github.com>
2023-09-30 16:23:24 +0200

    starting to format day and date elements

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-let currentDate = new Date();
-let h1 = document.querySelector("h1");
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  //let h1 = document.querySelector("h1");
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentDate.getDay()];
-let time = currentDate.getHours();
-let minutes = currentDate.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-h1.innerHTML = `${day} ${time}:${minutes}`;
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function displayTemp(response) {
   let temp = Math.round(response.data.main.temp);
